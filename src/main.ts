dd196ffeeadf8c1cbd13760dcce59253e77ea053
tianlun <wangtl@asp.citic.com>
2019-07-25 11:11:48 +0800

    项目框架搭建

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,9 @@ import App from './App.vue';
 import router from './router';
 import store from './store';
 
+import 'normalize.css/normalize.css'; // 样式初始化，并保证各浏览器默认HTML元素样式的统一
+import 'amfe-flexible'; // 动态设置rem的基准值
+
 Vue.config.productionTip = false;
 
 new Vue({
